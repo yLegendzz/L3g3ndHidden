8c34f45ee93bf4013dcbda444565dfbea8b09558
yLegendzMaker <86037655+yLegendzz@users.noreply.github.com>
2023-10-08 08:01:19 -0300

    Create index.ts

---
+++ hidden-channels/src/index.ts
@@ -0,0 +1,62 @@
+import { findByName, findByProps } from "@vendetta/metro";
+import { constants, React } from "@vendetta/metro/common";
+import { instead, after } from "@vendetta/patcher";
+import HiddenChannel from "./HiddenChannel";
+
+let patches = [];
+
+const Permissions = findByProps("getChannelPermissions", "can");
+const Router = findByProps("transitionToGuild");
+const Fetcher = findByProps("stores", "fetchMessages");
+const { ChannelTypes } = findByProps("ChannelTypes");
+const {getChannel} = findByProps("getChannel");
+
+const skipChannels = [
+    ChannelTypes.DM, 
+    ChannelTypes.GROUP_DM, 
+    ChannelTypes.GUILD_CATEGORY
+]
+
+function isHidden(channel: any | undefined) {
+    if (channel == undefined) return false;
+    if (typeof channel === 'string')
+        channel = getChannel(channel);
+    if (!channel || skipChannels.includes(channel.type)) return false;
+    channel.realCheck = true;
+    let res = !Permissions.can(constants.Permissions.VIEW_CHANNEL, channel);
+    delete channel.realCheck;
+    return res;
+}
+function onLoad() {
+    const MessagesConnected = findByName("MessagesWrapperConnected", false);
+    
+    patches.push(after("can", Permissions, ([permID, channel], res) => {
+        if (!channel?.realCheck && permID === constants.Permissions.VIEW_CHANNEL) return true;
+        return res;
+    }));
+
+    patches.push(instead("transitionToGuild", Router, (args, orig) => {
+        const [_, channel] = args;
+        if (!isHidden(channel) && typeof orig === "function") orig(args);
+    }));
+
+    patches.push(instead("fetchMessages", Fetcher, (args, orig) => {
+        const [channel] = args;
+        if (!isHidden(channel) && typeof orig === "function") orig(args);
+    }));
+
+    patches.push(instead("default", MessagesConnected, (args, orig) => {
+        const channel = args[0]?.channel;
+        if (!isHidden(channel) && typeof orig === "function") return orig(...args);
+        else return React.createElement(HiddenChannel, {channel});
+    }));
+}
+
+export default {
+    onLoad,
+    onUnload: () => {
+        for (const unpatch of patches) {
+            unpatch();
+        };
+    }
+}
